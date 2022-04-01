64ba88b231029177f7024d3d83f4fe01d49edfe0
prernapawar <100364823+prernapawar@users.noreply.github.com>
2022-04-01 16:20:20 +0530

    even array program

---
+++ even array/src/bridgelabz/EvenPosition.java
@@ -0,0 +1,15 @@
+package bridgelabz;
+public class EvenPosition {  
+    public static void main(String[] args) {  
+  
+        //Initialize array  
+        int [] arr = new int [] {3, 6, 9, 14, 18,20};  
+  
+        System.out.println("Elements of given array present on even position: ");  
+        //Loop through the array by incrementing value of i by 2  
+        //Here, i will start from 1 as first even positioned element is present at position 1.  
+        for (int i = 1; i < arr.length; i = i+2) {  
+            System.out.println(arr[i]);  
+        }  
+    }  
+}  
